e9bfdd9cfb5114a60f79156ad23f62669951a8d4
Jhon Chiguay <informaticojhon@gmail.com>
2024-09-18 10:11:20 -0300

    Creación ListaJuegos.vue e instalación de bootstrap para mejorar el aspecto visual

---
+++ src/components/ListaJuegos.vue
@@ -0,0 +1,39 @@
+<template>
+    <table>
+      <thead>
+        <tr>
+          <th>Código</th>
+          <th>Nombre</th>
+          <th>Stock</th>
+          <th>Precio</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="juego in juegos" :key="juego.id">
+          <td>{{ juego.codigo }}</td>
+          <td>{{ juego.nombre }}</td>
+          <td>{{ juego.stock }}</td>
+          <td>{{ juego.precio }}</td>
+          <td>
+            <button @click="modificarStock(juego.id, juego.stock - 1)" :disabled="juego.stock <= 0">-</button>
+            <button @click="modificarStock(juego.id, juego.stock + 1)">+</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </template>
+  
+  <script>
+  export default {
+    props: ['juegos'],
+    methods: {
+      modificarStock(id, stock) {
+        if (stock >= 0) {
+          this.$emit('update-stock', { id, stock });
+        }
+      }
+    }
+  };
+  </script>
+  
